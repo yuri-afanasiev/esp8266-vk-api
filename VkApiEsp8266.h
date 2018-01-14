dd4b3d68e5116996e97e38814e2084ab88082287
Yuriy Afanasiev <33378584+yuri-afanasiev@users.noreply.github.com>
2018-01-14 18:50:59 +0300

    Add files via upload

--- VkApiEsp8266.h
+++ VkApiEsp8266.h
@@ -1,6 +1,3 @@
- 
- 
-
 #include <ArduinoJson.h> 
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
@@ -28,6 +25,7 @@ public:
 	int like_photo;
 	int messages_new;
 	int participants;
+	int online;
 	int error;
 	String  last_message;
 	WiFiClientSecure client_vk;
@@ -53,7 +51,7 @@ public:
 				error = 1;
 			     }
 		}
-		   delay(500);
+		   delay(200);
 	 while (client_vk.available()) {
 			line = client_vk.readStringUntil('\r');
 			DynamicJsonBuffer  jsonBuffer;
@@ -94,7 +92,7 @@ public:
 				error = 1;
 			}
 		}
-		delay(500);
+		delay(200);
 	  while (client_vk.available()) {
 			line = client_vk.readStringUntil('\r');
 			DynamicJsonBuffer  likeBuffer;
@@ -127,7 +125,7 @@ public:
 				error = 1;
 			}
 		}
-		client_vk.stop();
+		//client_vk.stop();
 	} 
 	void message(int id_vk, String token, int message) {
 		// Отправляет сообщение https://vk.com/dev/messages.send
@@ -174,7 +172,7 @@ public:
 				error = 1;
 			}
 		}
-		delay(500);
+		delay(200);
 		while (client_vk.available()) {
 			line = client_vk.readStringUntil('\r');
 			DynamicJsonBuffer  vkBuffer;
@@ -206,7 +204,7 @@ public:
 				error = 1;
 			}
 		}
-		delay(500);
+		delay(200);
 		while (client_vk.available()) {
 			line = client_vk.readStringUntil('\r');
 			DynamicJsonBuffer  vkBuffer;
@@ -240,7 +238,7 @@ public:
 				error = 1;
 			}
 		}
-		delay(500);
+		delay(200);
 		while (client_vk.available()) {
 			line = client_vk.readStringUntil('\r');
 			DynamicJsonBuffer  likeBuffer;
@@ -251,6 +249,193 @@ public:
 		client_vk.stop();
 	}
  
-};
 
+
+	void users_online(String users) {
+		//https://vk.com/dev/users.get
+		String url = "/method/users.get?";
+		url += "user_ids=";
+		url += users;
+		url += "&fields=online&name_case=Nom&v=5.69";
+		if (!client_vk.connect(host_vk, httpsPort)) {
+			error = 1;
+		}
+		else {
+			error = 0;
+			if (client_vk.verify(fingerprint_vk, host_vk)) {
+				client_vk.print(String("GET ") + url + " HTTP/1.1\r\n" +
+					"Host: " + host_vk + "\r\n" +
+					"Connection: close\r\n\r\n");
+			}
+			else {
+				error = 1;
+			}
+		}
+		delay(1000);
+		while (client_vk.available()) {
+			line = client_vk.readStringUntil('\r');
+			DynamicJsonBuffer  likeBuffer;
+
+			JsonObject& root = likeBuffer.parseObject(line);
+			online = root["response"][0]["online"];
+		}
+		client_vk.stop();
+	}
+
+
+};
+class vk_api_host {
+
+private:
+	const int httpPort = 80;
+	String host_vk;
+	String wurl;
+
+	String line;
+	String object_type;
+	int owner_id;
+	int item_id;
+
+
+public:
+	int likes, comments, reposts, views;
+	int like_photo;
+	int messages_new;
+	int participants;
+	int error;
+	String  last_message;
+	WiFiClient  client_vk_host;
+
+	void server(String host) {
+		host_vk = host;
+	}
+
+	void url(String host) {
+		wurl = host;
+	}
+	void wall(String id) {
+		String url = wurl;
+		url += "?id=";
+		url += id;
+		url += "&ri=30";
+		 
+
+		if (client_vk_host.connect(host_vk.c_str(), httpPort)) {
+			client_vk_host.print(String("GET ") + url + " HTTP/1.1\r\n" +
+				"Host: " + host_vk + "\r\n" +
+				"Connection: close\r\n\r\n");
+			 
+		}
+		delay(500);
+		while (client_vk_host.available()) {
+			line = client_vk_host.readStringUntil('\r');
+			DynamicJsonBuffer  jsonBuffer;
+			JsonObject& root = jsonBuffer.parseObject(line);
+			likes = root["likes"];//информация о лайках к записи
+			comments = root["comments"];//информация о комментариях к записи
+			reposts = root["reposts"];//информация о репостах записи 
+			views = root["views"];//информация о просмотрах записи
+		}
+
+
+
+	}
+
+	void like(String object_type, int owner_id, int item_id) {
+		//https://vk.com/dev/likes.getList  
+		//object_type   post — запись на стене  photo — фотография
+		//owner_id      идентификатор владельца Like-объекта: id пользователя   
+		//item_id       идентификатор Like-объекта      
+
+	 
+	 
+		String url = wurl;
+		url += "?owner_id=";
+		url += owner_id;
+		url += "&item_id=";
+		url += item_id;
+		url += "&type=";
+		url += object_type;
+		url += "&ri=31";
+
+
+		if (client_vk_host.connect(host_vk.c_str(), httpPort)) {
+			client_vk_host.print(String("GET ") + url + " HTTP/1.1\r\n" +
+				"Host: " + host_vk + "\r\n" +
+				"Connection: close\r\n\r\n");
+		 
+		}
+		delay(500);
+		while (client_vk_host.available()) {
+			line = client_vk_host.readStringUntil('\r');
+			DynamicJsonBuffer  jsonBuffer;
+			JsonObject& root = jsonBuffer.parseObject(line);
+			like_photo = root["like_photo"];
+		}
+
+		 
+
+			 
+ 
+
+}
+
+
+	void group(int group_id) {
+		//https://vk.com/dev/groups.getMembers
+		//group_id       id группы 
+		String url = wurl;
+		url += "?group_id=";
+		url += group_id;
+		url += "&ri=32";
+
+
+		if (client_vk_host.connect(host_vk.c_str(), httpPort)) {
+			client_vk_host.print(String("GET ") + url + " HTTP/1.1\r\n" +
+				"Host: " + host_vk + "\r\n" +
+				"Connection: close\r\n\r\n");
+
+		}
+		delay(500);
+		while (client_vk_host.available()) {
+			line = client_vk_host.readStringUntil('\r');
+			DynamicJsonBuffer  jsonBuffer;
+			JsonObject& root = jsonBuffer.parseObject(line);
+			participants = root["participants"];
+		}
+
+
+
+	}
+
+
+	void new_messages(String token) {
+		//https://vk.com/dev/account.getCounters
+
+		String url = wurl;
+		url += "?token=";
+		url += token;
+		url += "&ri=33";
+
+
+		if (client_vk_host.connect(host_vk.c_str(), httpPort)) {
+			client_vk_host.print(String("GET ") + url + " HTTP/1.1\r\n" +
+				"Host: " + host_vk + "\r\n" +
+				"Connection: close\r\n\r\n");
+
+		}
+		delay(500);
+		while (client_vk_host.available()) {
+			line = client_vk_host.readStringUntil('\r');
+			DynamicJsonBuffer  jsonBuffer;
+			JsonObject& root = jsonBuffer.parseObject(line);
+			messages_new = root["messages_new"];
+		}
+
+
+
+	}
+
+
+};
  
